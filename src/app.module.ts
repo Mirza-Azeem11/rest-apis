458d6fd29152dbda62bba8e0d0e283a725fb4779
Mirza-Azeem11 <30124618@students.southwales.ac.uk>
2025-03-18 22:21:07 +0500

    Setup Database Service
    Create Shared Service for managing database and other things

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { BookModule } from "./book/book.module";
+import { SharedModule } from './shared/shared.module';
+import { ConfigModule } from '@nestjs/config';
+import { config } from 'dotenv';
+import typeorm from './config/databaseConfig';
+
+
+config();
 
 @Module({
-  imports: [BookModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env', `.env.${process.env.NODE_ENV}`],
+      load: [typeorm],
+    }),
+      BookModule,SharedModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/databaseConfig.ts
@@ -0,0 +1,37 @@
+import { registerAs } from "@nestjs/config";
+
+import { DataSource, DataSourceOptions } from "typeorm";
+
+import { config } from "dotenv";
+import * as console from "node:console";
+import * as process from "node:process";
+// loading the environment variables
+config();
+// loading the environment variables according to the path of env
+config({ path: `.env.${process.env.NODE_ENV}` });
+//const prod = process.env.NODE_ENV === "production";
+const prod = process.env.NODE_ENV === "production";
+
+console.log(process.env.DATABASE_HOST);
+const dataSourceOptions: DataSourceOptions = {
+  type: "mysql",
+  host: process.env.DATABASE_HOST,
+  port: 3360,
+  username: process.env.DATABASE_USER_NAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  maxQueryExecutionTime: 1000,
+  cache: false,
+  logging: prod ? ["query", "error", "log"] : "all",
+  logger: "advanced-console",
+  entities: [__dirname + "/../**/entities/*.{ts,js}"],
+  migrations: [__dirname + "/../**/migrations/**/*{.ts,.js}"],
+  migrationsTableName: "typeorm_migrations",
+  synchronize: false, // never use TRUE in production!
+  // debug: true,
+  //acquireTimeout: 3000, // 3 seconds
+  connectTimeout: 10000,
+};
+export default registerAs("typeorm", () => dataSourceOptions);
+
+export const connectionSource = new DataSource(dataSourceOptions);
